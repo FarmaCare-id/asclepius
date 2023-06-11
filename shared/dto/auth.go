a81aba0686ac8f0613af74357d3d5c3f673c8646
eugeniusms <eugeniusms@gmail.com>
2023-06-11 23:27:37 +0700

    feat: add user props

--- shared/dto/auth.go
+++ shared/dto/auth.go
@@ -11,14 +11,30 @@ const (
 	Credential UserLoginType = "Credential"
 )
 
+const (
+	RoleUser       UserRole = "user"
+	RoleDoctor     UserRole = "doctor"
+	RolePharmacist UserRole = "pharmacist"
+	RoleAdmin      UserRole = "admin"
+)
+
 type (
 	UserLoginType string
+	UserRole string
 	User          struct {
 		ID                uint          `gorm:"primaryKey;autoIncrement"`
 		Email             string        `gorm:"column:email;unique;not null"`
 		Fullname          string        `gorm:"column:fullname"`
 		HashedPassword    string        `gorm:"column:hashed_password"`
+		Role           	  UserRole      `gorm:"column:role;default:user"`
 		Type              UserLoginType `gorm:"column:type;default:Credential"`
+		Weight			  float64		`gorm:"column:weight"`
+		Height			  float64		`gorm:"column:height"`
+		Age				  int			`gorm:"column:age"`
+		NoSip			  string		`gorm:"column:no_sip"`
+		NoSipa			  string		`gorm:"column:no_sipa"`
+		Specialist		  string		`gorm:"column:specialist"`
+		Title 			  string		`gorm:"column:title"`
 		CreatedAt         time.Time
 		UpdatedAt         time.Time
 		DeletedAt         gorm.DeletedAt
